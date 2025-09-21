144691badf55c6b704ba8431e4cfffd86cd55a58
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-09-21 15:55:53 +0500

    i

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-   var arr = [23,43,12,65,21] 
+add    var arr = [23,43,12,65,21] 
     var max = 0
     for(i = 0;i < arr.length ; i++){
         if(arr[i]>max){
@@ -7,4 +7,17 @@
        
     }
     console.log("Largest number is" ,max)
+
+
+
+
+   var arr = [23,43,12,65,21] 
+    var min = 1000000
+    for(i = 0;i < arr.length ; i++){
+        if(arr[i]<min){
+            min=arr[i]
+        }
+       
+    }
+    console.log("Smallest number is" ,min)
 
