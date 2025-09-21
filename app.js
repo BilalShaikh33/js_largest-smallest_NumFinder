542700441c3fbedba82e67c07168d007bc5a7f64
BilalShaikh33 <bilalshaikhh339@gmail.com>
2025-09-21 15:47:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+   var arr = [23,43,12,65,21] 
+    var max = 0
+    for(i = 0;i < arr.length ; i++){
+        if(arr[i]>max){
+            max=arr[i]
+        }
+       
+    }
+    console.log("Largest number is" ,max)
+
